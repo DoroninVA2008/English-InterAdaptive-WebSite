40f85a055188148d4e1f19de8c257e0e9e7e5c5f
DoroninVA2008 <hotlive95334@gmail.com>
2026-05-03 21:55:07 +0500

    ComPush after adding 2 block in this InterAdaptive WebSite 6

--- English-InterAdaptive-WebSite/WebPack+BootStrap+CSS/src/js/script.js
+++ English-InterAdaptive-WebSite/WebPack+BootStrap+CSS/src/js/script.js
@@ -1,3 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import '../css/style.css'
+
+document.getElementById("starts").addEventListener("click", function() {
+    document.getElementById("features").scrollIntoView({ behavior: "smooth" });
+});
